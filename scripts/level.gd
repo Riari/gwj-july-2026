fd0b0d648f8a6872f4726c1515829f71bebaeee7
Rick Mann <e@rick.me.uk>
2026-07-15 20:19:34 +0100

    Add base level and living room level

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,22 +1,11 @@
 extends Node
 
-func _ready() -> void:
-	GameState.reset_score()
-
 @export var game_over_music: AudioStream
 
-@onready var fade_out: ColorRect = %FadeOut
-@onready var game_over_label: Label = %GameOverLabel
+@onready var in_game_ui: InGameUI = %InGameUI
 
 func _on_player_character_died() -> void:
-	ProjectMusicController.fade_out(1.0)
-	
-	var fade_tween := fade_out.create_tween()
-	fade_tween.tween_interval(0.2)
-	fade_tween.tween_property(fade_out, "color:a", 1.0, 1.0)
-	fade_tween.play()
-	await fade_tween.finished
-	game_over_label.visible = true
+	await in_game_ui.on_player_character_died()
 
 	if game_over_music:
 		ProjectMusicController.play_stream(game_over_music)
---
+++ scripts/ui/in_game_ui.gd
@@ -0,0 +1,21 @@
+class_name InGameUI extends Node
+
+@onready var fade_rect: ColorRect = %FadeRect
+@onready var game_over_label: Label = %GameOverLabel
+
+func _ready() -> void:
+	GameState.reset_score()
+	fade_rect.color.a = 1.0
+	var fade_tween := fade_rect.create_tween()
+	fade_tween.tween_property(fade_rect, "color:a", 0.0, 0.5)
+	fade_tween.play()
+
+func on_player_character_died() -> void:
+	ProjectMusicController.fade_out(1.0)
+	
+	var fade_tween := fade_rect.create_tween()
+	fade_tween.tween_interval(0.2)
+	fade_tween.tween_property(fade_rect, "color:a", 1.0, 1.0)
+	fade_tween.play()
+	await fade_tween.finished
+	game_over_label.visible = true
