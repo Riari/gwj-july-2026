036172f8855cd1162319254d9f5b30d53caaa054
Rick Mann <e@rick.me.uk>
2026-07-16 23:59:25 +0100

    Fake pixel perfect score shadow because Godot can't do it :(

--- scripts/ui/hud.gd
+++ scripts/ui/hud.gd
@@ -1,12 +1,14 @@
 class_name HUD extends Control
 
 @onready var score_label: Label = %ScoreLabel
+@onready var score_label_shadow: Label = %ScoreLabelShadow
 
 var current_score := 0
 
 func _process(_delta):
 	if current_score != GameState.get_score():
 		score_label.text = "Score: " + str(GameState.get_score())
+		score_label_shadow.text = "Score: " + str(GameState.get_score())
 		current_score = GameState.get_score()
 
 func on_player_character_init(health: int) -> void:
