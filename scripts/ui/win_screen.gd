f424447c784c05a5a5ba6731fd9025a906435a2a
Rick Mann <e@rick.me.uk>
2026-07-19 19:47:56 +0100

    Add buttons to win screen

--- scripts/ui/win_screen.gd
+++ scripts/ui/win_screen.gd
@@ -6,6 +6,17 @@ extends Control
 @onready var medal_multiplier_label: Label = %MedalMultiplierLabel
 @onready var final_score_label: RichTextLabel = %FinalScoreLabel
 @onready var new_high_score_label: Label = %NewHighScoreLabel
+@onready var main_menu_button: Button = %MainMenuButton
+@onready var retry_button: Button = %RetryButton
+@onready var next_level_button: Button = %NextLevelButton
+@onready var confirmation_window: ConfirmationOverlaidWindow = %ConfirmationWindow
+@onready var scene_loader_node = get_tree().root.get_node_or_null(^"SceneLoader")
+
+var _main_menu_scene_path: String = "res://scenes/menus/main_menu/main_menu_with_animations.tscn"
+var _current_level_path: String = ""
+var _next_level_path: String = ""
+
+var _next_scene_path: String = ""
 
 func _ready() -> void:
 	var level_index := GameState.get_last_level_index()
@@ -33,3 +44,31 @@ func _ready() -> void:
 	medal_multiplier_label.text = "Score Multiplier: " + str(score_multiplier) + "x"
 	final_score_label.parse_bbcode("Final Score: [color=green]" + str(final_score) + "[/color]")
 	new_high_score_label.visible = is_new_high_score
+
+	_current_level_path = LevelUtils.LEVEL_PATHS[level_index]
+	var next_level_index := level_index + 1
+	if next_level_index < LevelUtils.LEVEL_PATHS.size():
+		_next_level_path = LevelUtils.LEVEL_PATHS[next_level_index]
+	else:
+		next_level_button.visible = false
+
+	main_menu_button.pressed.connect(_on_main_menu_button_pressed)
+	retry_button.pressed.connect(_on_retry_button_pressed)
+	next_level_button.pressed.connect(_on_next_level_button_pressed)
+	
+	confirmation_window.visible = false
+	confirmation_window.confirmed.connect(_on_confirmed)
+
+func _on_main_menu_button_pressed() -> void:
+	_next_scene_path = _main_menu_scene_path
+	confirmation_window.visible = true
+
+func _on_retry_button_pressed() -> void:
+	_next_scene_path = _current_level_path
+	confirmation_window.visible = true
+
+func _on_next_level_button_pressed() -> void:
+	scene_loader_node.load_scene(_next_level_path)
+
+func _on_confirmed() -> void:
+	scene_loader_node.load_scene(_next_scene_path)
---
+++ scripts/utils/level_utils.gd
@@ -0,0 +1,6 @@
+﻿class_name LevelUtils
+
+const LEVEL_PATHS: Array[String] = [
+	"res://scenes/game_scene/levels/level_living_room.tscn",
+	"res://scenes/game_scene/levels/level_kitchen.tscn"
+]
