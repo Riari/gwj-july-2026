3d14a98bb127b7a22d0173195f033abc37883349
Rick Mann <e@rick.me.uk>
2026-07-19 20:59:35 +0100

    Clean up for final submission

--- scripts/utils/level_utils.gd
+++ scripts/utils/level_utils.gd
@@ -1,6 +1,5 @@
 ﻿class_name LevelUtils
 
 const LEVEL_PATHS: Array[String] = [
-	"res://scenes/game_scene/levels/level_living_room.tscn",
-	"res://scenes/game_scene/levels/level_kitchen.tscn"
+	"res://scenes/game_scene/levels/level_living_room.tscn"
 ]
