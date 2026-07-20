3c3f54c8a66a10231a27d129b5ecda4adca88f24
Rick Mann <e@rick.me.uk>
2026-07-20 21:09:57 +0100

    Code style

--- scripts/components/health_component.gd
+++ scripts/components/health_component.gd
@@ -4,17 +4,17 @@ class_name HealthComponent extends Component
 @export var hurt_sprite: Node2D
 @export var hurt_flash_duration: float = 0.1
 
-var current_health: int
+var _current_health: int
 
 func init(character: Character) -> void:
 	super.init(character)
-	current_health = max_health
-	_character.on_health_init.emit(current_health)
+	_current_health = max_health
+	_character.on_health_init.emit(_current_health)
 
 func on_character_hurt(instigator: Character, damage: int, _knockback_multiplier: float = 1.0) -> void:
-	current_health -= damage
+	_current_health -= damage
 
-	if current_health <= 0:
+	if _current_health <= 0:
 		_on_died(instigator)
 
 	hurt_sprite.material.set_shader_parameter("flash_active", true)
@@ -25,7 +25,7 @@ func on_character_picked_up(pickup: Pickup) -> void:
 	_on_heal(pickup.heal_value)
 
 func _on_heal(amount: int) -> void:
-	current_health += amount
+	_current_health += amount
 	_character.on_healed.emit(amount)
 
 func _on_died(instigator: Character) -> void:
